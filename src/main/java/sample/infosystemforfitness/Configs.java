5689534b1fda659123d83e23eef39c9cf11fffce
Igor <nefedov.igor@vk.com>
2021-09-29 14:24:23 +0300

    Обработан процесс добавления пользователя в базу данных.

--- src/main/java/sample/infosystemforfitness/Configs.java
+++ src/main/java/sample/infosystemforfitness/Configs.java
@@ -5,6 +5,6 @@ public class Configs {
     protected String dbPort = "3306";
     protected String dbUser = "root";
     protected String dbPass = "Nbnfy2003";
-    protected String dbName = "ISForFitnessCentre";
+    protected String dbName = "isforfitnesscentre";
 
 }
