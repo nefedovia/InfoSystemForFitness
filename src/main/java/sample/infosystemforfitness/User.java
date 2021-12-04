6d7eec0e217ec2e1d54ab34118540c28f42e73b2
YHNRH <kdog2105@mail.ru>
2021-12-04 23:40:02 +0300

    Исправлены неточности

--- src/main/java/sample/infosystemforfitness/User.java
+++ src/main/java/sample/infosystemforfitness/User.java
@@ -1,94 +1,81 @@
 package sample.infosystemforfitness;
 
 public class User {
-    private static String firstName;
-    private static String lastName;
-    private static String userName;
+    private  String firstName;
+    private  String lastName;
+    private  String userName;
     private String password;
-    private static String gender;
-    private static String phone;
-    private static String role;
-    private static int id;
+    private  String gender;
+    private  String phone;
+    private  String role;
+    private  int id;
     private static User user;
 
-    public User(String firstName, String lastName, String userName, String password, String gender, String phone) {
+    public User(String firstName, String lastName, String userName, String password, String gender, String phone, String role) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.userName = userName;
         this.password = password;
         this.gender = gender;
         this.phone = phone;
+        this.role = role;
     }
 
     public static User instance(){
         if(user == null){
-            user = new User("","","","","","");
+            user = new User("","","","","","","");
         }
         return user;
     }
-
-
     public static void init(String firstName, String lastName, String userName, String password,  String gender,  String phone , String role){
         if(user == null){
-            user = new User(firstName,lastName,userName,password,gender,phone);
+            user = new User(firstName,lastName,userName,password,gender,phone,role);
         }
     }
 
 
-    public User() {
+
+
+
+
+
+
+
+
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
 
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getRole() {        return role;    }
-
-    public void setRole(String role) {        this.role = role;    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public String getUserName() {
         return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getGender() {
         return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public String getFirstName() {
+        return firstName;
     }
 }
