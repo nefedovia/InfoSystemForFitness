4a72121019af190f0b19ae40234422d4c27fa1fb
YHNRH <kdog2105@mail.ru>
2021-12-04 20:16:52 +0300

    Исправлены ошибки, добавлена авторизация

--- src/main/java/sample/infosystemforfitness/Const.java
+++ src/main/java/sample/infosystemforfitness/Const.java
@@ -2,7 +2,6 @@ package sample.infosystemforfitness;
 
 public class Const {
     public static final String USER_TABLE = "users";
-
     public static final String USERS_ID = "idusers";
     public static final String USERS_FIRSTNAME = "firstname";
     public static final String USERS_LASTNAME = "lastname";
